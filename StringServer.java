cad8cf227366c6657a089416ca948702b580f6d6
kyc013 <147003854+kyc013@users.noreply.github.com>
2023-10-20 22:46:14 -0700

    Update StringServer.java

--- StringServer.java
+++ StringServer.java
@@ -2,8 +2,7 @@ import java.io.IOException;
 import java.net.URI;
 
 class Handler implements URLHandler {
-    // The one bit of state on the server: a number that will be manipulated by
-    // various requests.
+
     int num = 0;
     string string;
 
