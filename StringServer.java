93805efc7bfeb31e7836b55de01126a0c1202f19
kyc013 <147003854+kyc013@users.noreply.github.com>
2023-10-21 17:44:57 -0700

    Update StringServer.java

--- StringServer.java
+++ StringServer.java
@@ -3,21 +3,33 @@ import java.net.URI;
 
 class Handler implements URLHandler {
 
-    int num = 0;
-    string string;
+private StringBuilder String = new StringBuilder();
+private int num = 0;
 
     public String handleRequest(URI url) {
 
-            if (url.getPath().contains("/add")) {
-                String[] parameters = url.getQuery().split("-message?s=");
-                    num++;
-                    return String.format(num, parameters[1]/n);
+
+        if (url.getPath().equals("/")) {
+            return String.toString();
+        } else if (url.getPath().equals("/add-message")) {
+
+                String[] parameters = url.getQuery().split("=");
+                    if (parameters.length == 2 && parameters[0].equals("s")){
+                        String message = parameters[1];
+                        num++;
+
+                    String.append(num).append(". ").append(message).append("\n");
+                    return String.toString();
+                    }
+        }
+                    
+                    return "Invalid request";
             }
 
-    
-}
+    }
 
-class NumberServer {
+
+class StringServer {
     public static void main(String[] args) throws IOException {
         if(args.length == 0){
             System.out.println("Missing port number! Try any number between 1024 to 49151");
